7f4425cc9c52cd0d3a32bd0bd44751250e9663c2
Serhii Smoliak <serhiysmoliak@gmail.com>
2024-10-24 11:36:42 +0300

    created simple layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './layout/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Homepage from './pages/Homepage';
+import Servises from './pages/Services';
+import ServiseEntry from './pages/ServiceEntry';
+import Publications from './pages/Publications';
+import PublicationEntry from './pages/PublicationEntry';
+import Team from './pages/Team';
+import TeamEntry from './pages/TeamEntry';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Homepage />
+      },
+      {
+        path: '/services',
+        element: <Servises />
+      },
+      {
+        path: '/services/:id',
+        element: <ServiseEntry />
+      },
+      {
+        path: '/publications',
+        element: <Publications />
+      },
+      {
+        path: '/publications/:id',
+        element: <PublicationEntry />
+      },
+      {
+        path: '/team',
+        element: <Team />
+      },
+      {
+        path: '/team/:id',
+        element: <TeamEntry />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/about',
+        element: <About />
+      }
+    ]
+  }
+]);
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <RouterProvider router={router} />
+  );
+};
 
-export default App
+export default App;
